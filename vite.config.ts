bf5471c8f63deb3f2c5fa2194d5be689d0ecfb33
Vivian Alexander Lyngdoh Nonglait <valnilyo@gmail.com>
2026-03-11 22:55:48 +0530

    initial

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools(), VitePWA({ registerType: 'autoUpdate' })],
+  base: '/eco/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
